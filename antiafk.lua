3074bec5cd1c82970a0d683b1f2dbfdd857455f6
Mr-Hacker-Man <mrhackerman038@gmail.com>
2025-05-14 17:27:47 -0700

    Add files via upload

---
+++ antiafk.lua
@@ -0,0 +1,7 @@
+-- Isaac's Anti-AFK
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    local vu = game:GetService("VirtualUser")
+    vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+end)
